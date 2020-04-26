80bca00881d0ac597d5660117b62899008bb8018
Sydney Anne Erickson <sydneyerickson@icloud.com>
2020-04-25 21:33:04 -0700

    Initial configs

---
+++ setup.sh
@@ -0,0 +1,16 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh"
+brew install nvim cmake python3
+python3 -m pip install --user --upgrade pynvim
+mkdir -p ~/.vim/bundle && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.config/nvim
+cat .config/nvim/init.vim > ~/.config/nvim/init.vim
+mkdir -p ~/.vim
+cat .vim/.ycm_extra_conf.py > ~/.vim/.ycm_extra_conf.py
+nvim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+
+git clone git@github.com:asnr/cmus.git -b asnr-master
+cd cmus
+./configure
+make
+make install
---
+++ sync.sh
@@ -0,0 +1,2 @@
+cat ~/.config/nvim/init.vim > .config/nvim/init.vim
+cat ~/.vim/.ycm_extra_conf.py > .vim/.ycm_extra_conf.py
